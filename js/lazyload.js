47d6a52f3b6cc5070a2146e11f7f41ea0427ebf8
罗远祥 <1141306760@qq.com>
2019-11-13 15:45:22 +0800

    Site updated: 2019-11-13 15:45:22

--- js/lazyload.js
+++ js/lazyload.js
@@ -1,3 +1,3 @@
-// build time:Wed Nov 13 2019 14:57:36 GMT+0800 (GMT+08:00)
+// build time:Wed Nov 13 2019 15:45:19 GMT+0800 (GMT+08:00)
 (function(t){var n=Array.prototype.slice.call(document.querySelectorAll("img[srcset]"));function e(n){var e=n.getBoundingClientRect();var c=t.innerHeight||document.documentElement.clientHeight;return e.top>=0&&e.left>=0&&e.top<=c*1.5}function c(t,n){var e=new Image,c=t.getAttribute("src");e.onload=function(){t.srcset=c;n?n():null};e.srcset=c}function i(){for(var t=0;t<n.length;t++){if(e(n[t])){(function(t){var e=n[t];c(e,function(){n=n.filter(function(t){return e!==t})})})(t)}}}function o(t,n){clearTimeout(t.tId);t.tId=setTimeout(function(){t.call(n)},200)}i();t.addEventListener("scroll",function(){o(i,t)})})(this);
 //rebuild by neat 
